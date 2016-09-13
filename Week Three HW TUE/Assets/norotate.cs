e6e1f97a3242e3707bf44cafeaac1e920220d82c
yingtingyang <yangy389@newschool.edu>
2016-09-13 12:38:27 -0500

    box up

---
+++ Week Three HW TUE/Assets/norotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+public class norotate : MonoBehaviour
+{
+	 Quaternion rotation;
+	void Awake()
+	{
+		rotation = transform.rotation;
+	}
+	void LateUpdate()
+	{
+		transform.rotation = rotation;
+	}
+}
---
+++ Week Three HW TUE/Assets/trapdoorswitch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trapdoorswitch : MonoBehaviour {
+	float rotationspeed = 5;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//if I hit a specific key, open the trap door
+		if (Input.GetKey( KeyCode.W )){
+			print("open trapdoor!");
+			// rotatationspeed z to -90degree
+			transform.Rotate(Vector3.forward * rotationspeed);
+		}
+	}
+}
